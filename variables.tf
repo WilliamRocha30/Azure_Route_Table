c8704413ef61dee46ac5382afa70b09c4ff5e166
WilliamRocha30 <wr.rocha30@gmail.com>
2022-06-01 17:05:45 -0300

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "location" {
+  type = string
+}
+
+variable "rg_name" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "rt_name" {
+  type = string
+} 
+variable "tags" {
+  type = map
+}
